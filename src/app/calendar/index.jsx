dac7099a1301cc0251d6bda5836198c9254ca8db
Tom Dugan <thomas@aadwebb.com>
2020-10-08 17:02:01 -0500

    selected dayh change

--- src/app/calendar/index.jsx
+++ src/app/calendar/index.jsx
@@ -1,33 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import moment from "moment";
 import "./styles.css";
 
 export default function Calendar() {
-	const value = moment()
+	const [calendar, setCalendar] = useState([]);
+	const [value, setValue] = useState(moment());
+
 	const startDay = value.clone().startOf("month").startOf("week");
 	const endDay = value.clone().endOf("month").endOf("week");
-	const day = startDay.clone().subtract(1, "day");
-	const calendar = [];
 
-	while(day.isBefore(endDay, "day")) {
-		calendar.push(
-			Array(7)
-				.fill(0)
-				.map(() => day.add(1, "day").clone())
-		)
-	}
+	useEffect(() => {
+		const day = startDay.clone().subtract(1, "day");
+		const a = [];
+		while(day.isBefore(endDay, "day")) {
+			a.push(
+				Array(7)
+					.fill(0)
+					.map(() => day.add(1, "day").clone())
+			)
+		}
+		setCalendar(a);
+	}, [value]);
 
 	return (
 		<div className="calendar">
-			{
-				calendar.map(week => <div>
-					{
-						week.map(day => <div className="day">
+			{calendar.map((week) => (
+				<div>
+					{week.map((day) => (
+						<div className="day"
+							onClick={() => setValue(day)}
+						>
+							<div
+								className={value.isSame(day, "day") ? "selected" : ""}
+							>
 							{day.format("D").toString()}
-						</div>)
-					}
-				</div>)
-			}
+							</div>
+						</div>
+						))}
+				</div>
+			))}
 		</div>
 	);
 }
